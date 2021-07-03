c2591ced367b4f8d27ba8e9fc307eb9a37d27f32
AraiKenta <arai.kenta.infratop@gmail.com>
2021-07-03 09:57:31 +0000

    [modified]header

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
-  resources :books
+  resources :books, only: [:create, :index, :show, :edit, :update, :destroy]
   resources :users, only: [:show, :index, :edit, :update]
+  get "/homes/about" => "homes#about"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
