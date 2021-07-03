be113f8ab953a15c37ee444edebb1e3f64f71485
AraiKenta <arai.kenta.infratop@gmail.com>
2021-07-03 14:34:16 +0000

    [edit]_login.html.erb

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,8 +1,18 @@
 class BooksController < ApplicationController
   def create
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to book_path(@book)
+    else
+      @books = Book.all
+      @user = User.find(current_user.id)
+      render books_path
+    end
   end
 
   def index
+    @books = Book.all
+    @user = User.find(current_user.id)
   end
 
   def show
@@ -15,5 +25,11 @@ class BooksController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,13 @@
 class UsersController < ApplicationController
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to user_path(@user)
+    else
+      render :new
+    end
+  end
+
   def index
     @users = User.all
     @book = Book.new
